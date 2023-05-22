f422cf26d601052f9890660827f9fddf6206951c
Mimi <91705093+shypotatosage@users.noreply.github.com>
2023-05-22 11:11:39 +0700

    Initial Commit

---
+++ PetZone-IOS/PetZone_IOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  PetZone_IOSApp.swift
+//  PetZone-IOS
+//
+//  Created by MacBook Pro on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PetZone_IOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
