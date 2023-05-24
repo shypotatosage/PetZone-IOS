25092641dbca6067263232cd561c71c3df234550
CallistaEve <70805652+CallistaEve@users.noreply.github.com>
2023-05-24 19:37:07 +0700

    WatchOS Target

---
+++ PetZone-WatchOS Watch App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PetZone-WatchOS Watch App
+//
+//  Created by MacBook Pro on 24/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MainView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
