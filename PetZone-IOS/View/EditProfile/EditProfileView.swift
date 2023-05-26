125cd922ae3445d54e05df7527e4ff46ab8252f3
CallistaEve <70805652+CallistaEve@users.noreply.github.com>
2023-05-26 09:32:12 +0700

    EditProfileView

--- PetZone-IOS/View/EditProfile/EditProfileView.swift
+++ PetZone-IOS/View/EditProfile/EditProfileView.swift
@@ -8,97 +8,73 @@
 import SwiftUI
 import PhotosUI
 
-//struct EditProfileView: View {
-//    @State private var newUser = User.sampleUser
-//    
-//    @State private var selectedItem: PhotosPickerItem? = nil
-//    @State private var selectedImageData: Data? = nil
-//    
-//    var body: some View {
-//        NavigationView {
-//            ScrollView{
-//                VStack{
-//                    PhotosPicker(
-//                        selection: $selectedItem,
-//                        matching: .images,
-//                        photoLibrary: .shared()) {
-//                            Text("Buka Galeri")
-//                        }
-//                        .onChange(of: selectedItem) { newItem in
-//                            Task {
-//                                // Retrieve selected asset in the form of Data
-//                                if let data = try? await newItem?.loadTransferable(type: Data.self) {
-//                                    selectedImageData = data
-//                                }
-//                            }
-//                        }
-//                    
-//                    if let selectedImageData,
-//                       let uiImage = UIImage(data: selectedImageData) {
-//                        Image(uiImage: uiImage)
-//                            .resizable()
-//                            .scaledToFit()
-//                            .frame(width: 250, height: 250)
-//                    }else{
-//                        Image("defaultprofile")
-//                            .resizable()
-//                            .scaledToFit()
-//                            .frame(width: 250, height: 250)
-//                    }
-//                    
-//                }
-//                Group{
-//                    Text("Name")
-//                        .frame(
-//                            maxWidth: .infinity,
-//                            maxHeight: .zero,
-//                            alignment:  .topLeading)
-//                    HStack {
-//                        Image(systemName: "person.fill").foregroundColor(.gray)
-//                        TextField("Enter Your Name", text: $newUser.name)
-//                            .textFieldStyle(RoundedBorderTextFieldStyle()).font(.headline)
-//                    }
-//                }
-//                .padding(10)
-//                Group{
-//                    Text("Email")
-//                        .frame(
-//                            maxWidth: .infinity,
-//                            maxHeight: .zero,
-//                            alignment:  .topLeading)
-//                    HStack {
-//                        Image(systemName: "envelope.fill").foregroundColor(.gray)
-//                        TextField("Enter Your Name", text: $newUser.email)
-//                            .textFieldStyle(RoundedBorderTextFieldStyle()).font(.headline)
-//                    }
-//                }
-//                .padding(10)
-//                Group{
-//                    Text("Phone Number")
-//                        .frame(
-//                            maxWidth: .infinity,
-//                            maxHeight: .zero,
-//                            alignment:  .topLeading)
-//                    HStack {
-//                        Image(systemName: "phone.fill").foregroundColor(.gray)
-//                        TextField("Enter Your Name", text: $newUser.phone_number)
-//                            .textFieldStyle(RoundedBorderTextFieldStyle()).font(.headline)
-//                    }
-//                }
-//                .padding(10)
-//                Button("Save"){
-//                }
-//                .buttonStyle(DefaultButton())
-//                .bold()
-//            }
-//            .padding(20)
-//                .navigationTitle("Edit Profile")
-//        }
-//    }
-//}
-//
-//struct EditProfileView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        EditProfileView()
-//    }
-//}
+struct EditProfileView: View {
+    @State private var newUser = User.sampleUser
+    
+    @State private var selectedItem: PhotosPickerItem? = nil
+    @State private var selectedImageData: Data? = nil
+    
+    var body: some View {
+        NavigationView {
+            ScrollView{
+                VStack{
+                    Image("profilepict")
+                        .resizable()
+                        .frame(width: 250, height: 250)
+                        .cornerRadius(1000)
+                }
+                Group{
+                    Text("Name")
+                        .frame(
+                            maxWidth: .infinity,
+                            maxHeight: .zero,
+                            alignment:  .topLeading)
+                    HStack {
+                        Image(systemName: "person.fill").foregroundColor(.gray)
+                        TextField("Enter Your Name", text: $newUser.name)
+                            .textFieldStyle(RoundedBorderTextFieldStyle()).font(.headline)
+                    }
+                }
+                .padding(10)
+                Group{
+                    Text("Email")
+                        .frame(
+                            maxWidth: .infinity,
+                            maxHeight: .zero,
+                            alignment:  .topLeading)
+                    HStack {
+                        Image(systemName: "envelope.fill").foregroundColor(.gray)
+                        TextField("Enter Your Name", text: $newUser.email)
+                            .textFieldStyle(RoundedBorderTextFieldStyle()).font(.headline)
+                    }
+                }
+                .padding(10)
+                Group{
+                    Text("Phone Number")
+                        .frame(
+                            maxWidth: .infinity,
+                            maxHeight: .zero,
+                            alignment:  .topLeading)
+                    HStack {
+                        Image(systemName: "phone.fill").foregroundColor(.gray)
+                        TextField("Enter Your Name", text: $newUser.phone_number)
+                            .textFieldStyle(RoundedBorderTextFieldStyle()).font(.headline)
+                    }
+                }
+                .padding(10)
+                Button("Logout"){
+                }
+                .buttonStyle(DefaultButton())
+                .bold()
+            }
+            .padding(20)
+                .navigationTitle("Edit Profile")
+        }
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
