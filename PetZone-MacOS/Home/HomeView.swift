bcafd14abf769d97b3092aa415662ea4889db842
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2023-05-30 21:33:59 +0700

    Mac Os Filter Range Price Done

--- PetZone-MacOS/Home/HomeView.swift
+++ PetZone-MacOS/Home/HomeView.swift
@@ -73,13 +73,30 @@ struct HomeView: View {
                                     ForEach(dropDownList, id: \.self){ client in
                                         Button(client) {
                                             self.value = client
-                                            if(self.value == "none"){
-                                                hotelViewModel.searchText=""}
+                                            if(self.value == "None"){
+                                                hotelViewModel.minimumPrice=0}
                                             else if(self.value == "10.000-50.000"){
-                                                hotelViewModel.searchText="Pet zone"
+                                                hotelViewModel.minimumPrice = 10000
+                                                hotelViewModel.maximumPrice = 50000
+                                                print(hotelViewModel.rangePetHotels)
                                             }
                                             else if(self.value == "50.000-100.000"){
-                                                hotelViewModel.searchText="the paws"
+                                                hotelViewModel.minimumPrice = 50000
+                                                hotelViewModel.maximumPrice = 100000
+                                                print(hotelViewModel.rangePetHotels)
+                                            }
+                                            else if(self.value == "100.000-200.000"){
+                                                hotelViewModel.minimumPrice = 100000
+                                                hotelViewModel.maximumPrice = 200000
+                                                print(hotelViewModel.rangePetHotels)
+                                            }
+                                            else if(self.value == "200.000-500.000"){
+                                                hotelViewModel.minimumPrice = 200000
+                                                hotelViewModel.maximumPrice = 500000
+                                                print(hotelViewModel.rangePetHotels)
+                                            }
+                                            else{
+                                                hotelViewModel.minimumPrice=0
                                             }
                                         }
                                     }
