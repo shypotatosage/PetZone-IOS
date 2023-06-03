25c16df186782ebad188fb32faf9263fa1cc20d9
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2023-06-03 12:53:22 +0700

    Bug

--- PetZone-MacOS/Hotel/OrderDetailView.swift
+++ PetZone-MacOS/Hotel/OrderDetailView.swift
@@ -8,9 +8,24 @@ import SwiftUI
 
 //untuk tampilan order detail view
 struct OrderDetailView: View {
-  
+    @State var order : Order
     var body: some View {
-          Text("asndsandkas")
+            ScrollView{
+                VStack{
+                    Group{
+                        Image(order.pet_hotel.hotel_pic[0])
+                            .resizable()
+                            .scaledToFill()
+                            .frame(maxWidth: .infinity, maxHeight: 400)
+                            .clipped()
+                }  .padding([.bottom], 100)
+            }.background(Color.white)
     }
 }
 
+//untuk tampilan order detail preview
+struct OrderDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderDetailView(order: Order.sampleOrder)
+    }
+}
